3199cc3ef19d13d936b0816869b1699d4bbe2dc3
Stambulo <stambulo@yandex.ru>
2021-12-24 21:22:37 +0200

    Bottom Navigation + Navigation Component

--- app/src/main/java/com/stambulo/learningassistant/view/MainActivity.kt
+++ app/src/main/java/com/stambulo/learningassistant/view/MainActivity.kt
@@ -4,24 +4,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.stambulo.learningassistant.R
 import com.stambulo.learningassistant.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     val navController by lazy {
-        (supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment).navController
-    }
+        (supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment).navController }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavigationView = binding.bottomNav
         setupActionBarWithNavController(navController)
+        bottomNavigationView.setupWithNavController(navController)
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
-    }
+    override fun onSupportNavigateUp(): Boolean { return navController.navigateUp() }
 }
